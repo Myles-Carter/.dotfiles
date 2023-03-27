7a2091d23c5f26a01495ca0dfd67d9352e9c4e5a
Myles Carter <112006452+Myles-Carter@users.noreply.github.com>
2023-03-27 17:54:12 -0400

    Update linux.sh

--- bin/linux.sh
+++ bin/linux.sh
@@ -2,17 +2,17 @@
 
 if [[ "$uname" != "Linux" ]] 
 then #if the name is not linux
-	mkdir ~/.TRASH
-	if [ -f ~/.vimrc ]
+	mkdir ~/.TRASH #makes trash
+	if [ -f ~/.vimrc ] #if statement
 	then
 		mv ~/.vimrc ~/.bup_vimrc #rename
 		echo "Error! Wrong uname" >> linuxsetup.log #print error message and append it
 	fi
 	
-	touch ~/.vimrc
+	touch ~/.vimrc # adds vimrc
 	cat etc/.vimrc > ~/.vimrc #redirects
 
 	echo "source~/.dotfiles/etc/bashrc_custom" >> ~/.bashrc #adds the file to the end of the bashrc
-	echo "error wrong os" >> linuxsetup.log
+	echo "error wrong os" >> linuxsetup.log #prints errors
 	exit
 fi
